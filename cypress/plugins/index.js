af3eb2dc8c99cfab0f5f978c53f63fd3f4d54696
Arulo <arieldanx@gmail.com>
2020-07-21 11:37:45 +0200

    - Solve the issue with Redux DevTools
    - Add extension folder into repo
    - Reference it into the plugin/index.js file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,7 +15,24 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+const path = require('path')
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on('before:browser:launch', (browser, launchOptions) => {
+    console.log('launching browser %o', browser)
+
+    // only load React DevTools extension
+    // when opening Chrome in interactive mode
+    if (browser.family === 'chromium') {
+      // we could also restrict the extension
+      // to only load when "browser.isHeaded" is true
+      const extensionFolder = path.resolve(__dirname, '..', '2.17.0_0')
+
+      console.log('adding React DevTools extension from', extensionFolder)
+      launchOptions.args.push(`--load-extension=${extensionFolder}`)
+
+      return launchOptions
+    }
+  })
 }
+
+  //lmhkpmbekcpmknklioeibfkpmmfibljd
